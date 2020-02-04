43d74e1ba8b9083e31c211d14e73cb78ea4294f1
sanse <adrisanse.ss@gmail.com>
2020-02-04 16:25:26 +0100

    updated despertador.c

--- despertador.c
+++ despertador.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 int main(int argc, char* argv[]){
 
-
-        if (argc == 2){
+	
+	if (argc == 2){
 
                 printf("Falta el argumento del mensaje. Pruebe de nuevo.\n");
                 exit(2);
@@ -12,33 +13,48 @@ int main(int argc, char* argv[]){
         }
 
 
-        if (argc < 3){
-                printf("Error de argumentos. El uso es ./despertador [SEGUNDOS] [MENSAJE1] [MENSAJE2] ...\n");
-                exit(1);
-        }
+	if (argc < 3){
+		printf("Error de argumentos. El uso es ./despertador [SEGUNDOS] [MENSAJE1] [MENSAJE2] ...\n");
+		exit(1);
+	}
 
-        int segundos = atoi(argv[1]);
-        int segundos_tr = 0;
+	int segundos = atoi(argv[1]);
 
-        while (segundos != -1){
+	int segundos_tr = 0;
 
-                printf("Han pasado %d segundos y quedan %d segundos.\n", segundos_tr, segundos);
-                sleep(1);
-                segundos--;
-                segundos_tr++;
+	if (segundos < 0){
 
-        }
+		int i = 2;
+		int msgs = argc - 2;
+		while (msgs != 0){
 
-        int msgs = argc-2;
-        int i = 2;
-        //printf("Hay %d mensajes\n", msgs);
-        while (msgs != 0){
+			printf("%s\n", argv[i]);
+			msgs--;
+			i++;
+		}
 
-                printf("%s\n", argv[i]);
-                i++;
-                msgs--;
+		exit(1);
 
-        }
+	}
 
-        exit(0);
-}
+	while (segundos != -1){
+		
+		printf("Han pasado %d segundos y quedan %d segundos.\n", segundos_tr, segundos);
+		sleep(1);
+		segundos--;
+		segundos_tr++; 
+
+	}
+	
+	int msgs = argc-2;
+	int i = 2;
+	while (msgs != 0){
+
+		printf("%s\n", argv[i]);
+		i++;
+		msgs--;
+	
+	}
+
+	exit(0);
+} 
