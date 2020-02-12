8b2068769207069be2a67392d320efebb8f396e1
Adrián San Segundo <adriansanse@MacBook-Pro-de-Adrian.local>
2020-02-12 18:24:25 +0100

    fix demo

--- mypack.c
+++ mypack.c
@@ -22,6 +22,7 @@
 #define MSG3 "No se dispone de permisos para esta operación. No se dispone de permisos para crear o modificar fich_destino\n"
 #define MSG4 "Otro tipo de errores\n"
 
+
 struct s_info {
            char Tipo;                       // ='Z'
            char Compri;                     // ='N' (por defecto) o 'Y'. Indica si los datos del  
@@ -33,38 +34,75 @@ struct s_info {
 }sInfo;
 
 struct  s_header {
-           struct s_info InfoF;                 // Información del fichero
-           char RestoHeader[RESTO_HEADER];  // ToDo...  
+           struct s_info *InfoF;                 // Información del fichero
+           char RestoHeader[RESTO_HEADER];       // ToDo...  
 }sHeader;
 
 
 int main(int argc, char* argv[]){
+    struct s_header *s_h = malloc(TAM_S_HEADER);
+    s_h->InfoF = malloc(TAM_S_INFO);
 
     if (argc < 3){
         write(2, MSG1, strlen(MSG1));
         _exit(ERR1);
     }
 
-    int sourceFileID = open(argv[1], O_CREAT | O_RDONLY);
+    int sourceFileID = open(argv[1], O_RDONLY);
+    int destFileID = open(argv[2], O_CREAT | O_RDWR, 0644);
+
     if (sourceFileID == -1){
         write(2, MSG2, strlen(MSG2));
         _exit(ERR2);
     }
 
-    strcpy(sInfo.FileName, argv[1]);
-    if (sourceFileID == -2){
-        write(2, MSG2, strlen(MSG2));
+    if (access(argv[2], F_OK | W_OK) == -1){
+        write(2, MSG3, strlen(MSG3));
         _exit(ERR3);
     }
 
     //otros errores (ERR4)
 
-    int destFileID = open(argv[2], O_CREAT | O_RDWR | O_APPEND, 0644);
+    strcpy(s_h->InfoF->FileName, argv[1]);
+    s_h->InfoF->Compri = 'N';
+    s_h->InfoF->Tipo = 'Z';
+    s_h->InfoF->TamOri = lseek(sourceFileID, 0L, SEEK_END);
+    s_h->InfoF->TamComp = s_h->InfoF->TamOri;
+
+    lseek(sourceFileID, -0L, SEEK_SET);
+
+    int tam = (int)s_h->InfoF->TamOri;
+    char str[TAM_BUFFER];
+    sprintf(str, "%d", tam);
+
+    write(destFileID, "Tipo del comprimido: ", strlen("Tipo del comprimido: "));
+    write(destFileID, &s_h->InfoF->Compri, strlen(&s_h->InfoF->Compri));
+    write(destFileID, "\n", 1);
+
+    write(destFileID, "Nombre del archivo: ", strlen("Nombre del archivo: "));
+    write(destFileID, s_h->InfoF->FileName, strlen(s_h->InfoF->FileName));
+    write(destFileID, "\n", 1);
+
+    write(destFileID, "Tipo: ", strlen("Tipo: "));
+    write(destFileID, &s_h->InfoF->Tipo, strlen(&s_h->InfoF->Tipo)-1);
+    write(destFileID, "\n", 1);
+
+    write(destFileID, "Tamaño del fichero origen: ", strlen("Tamaño del fichero origen: "));
+    write(destFileID, str, strlen(str));
+    write(destFileID, "\n", 1);
+
+    write(destFileID, "Tamaño del fichero comprimido: ", strlen("Tamaño del fichero comprimido: "));
+    write(destFileID, str, strlen(str));
+    write(destFileID, "\n", 1);
+
+    write(destFileID, s_h->RestoHeader, strlen(s_h->RestoHeader));
+    write(destFileID, "\n", strlen("\n"));
+
     int n;
-    char buf[TAM_BUFFER];
-    while ((n = read(sourceFileID, buf, TAM_BUFFER)) > 0){
+    char buf[TAM_BUFFER];    
+
+    if ( (n = read(sourceFileID, buf, TAM_BUFFER)) > 0)
         write(destFileID, buf, n);
-    }
 
     close(sourceFileID);
     close(destFileID);
