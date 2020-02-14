de2aacb204cb9d1ebb71206f110ae067c4302ab2
Adrián San Segundo <adriansanse@MacBook-Pro-de-Adrian.local>
2020-02-14 10:09:34 +0100

    fixed demo

--- mypack.c
+++ mypack.c
@@ -34,14 +34,14 @@ struct s_info {
 };
 
 struct  s_header {
-           struct s_info *InfoF;                 // Información del fichero
+           struct s_info InfoF;                 // Información del fichero
            char RestoHeader[RESTO_HEADER];       // ToDo...  
 };
 
 
 int main(int argc, char* argv[]){
-    struct s_header *s_h = malloc(TAM_S_HEADER);
-    s_h->InfoF = malloc(TAM_S_INFO);
+
+    struct s_header header;
 
     if (argc < 3){
         write(2, MSG1, strlen(MSG1));
@@ -69,47 +69,18 @@ int main(int argc, char* argv[]){
     //¿header? campo a campo o de golpe? 
     //error 4 -> ________???
 
-    strcpy(s_h->InfoF->FileName, argv[1]);
-    s_h->InfoF->Compri = 'N';
-    s_h->InfoF->Tipo = 'Z';
-    s_h->InfoF->TamOri = lseek(sourceFileID, 0L, SEEK_END);
-    s_h->InfoF->TamComp = s_h->InfoF->TamOri;
-
-    lseek(sourceFileID, -0L, SEEK_SET);
-
-    int tam = (int)s_h->InfoF->TamOri;
-    char str[TAM_BUFFER];
-    sprintf(str, "%d", tam);
+    strcpy(header.InfoF.FileName, argv[1]);
+    header.InfoF.Compri = 'N';
+    header.InfoF.Tipo = 'Z';
 
 
-
-    write(destFileID, "Tipo del comprimido: ", strlen("Tipo del comprimido: "));
-    write(destFileID, &s_h->InfoF->Compri, 1);
+    write(destFileID, &header, sizeof(header));
     write(destFileID, "\n", 1);
-
-    write(destFileID, "Nombre del archivo: ", strlen("Nombre del archivo: "));
-    write(destFileID, s_h->InfoF->FileName, strlen(s_h->InfoF->FileName));
-    write(destFileID, "\n", 1);
-
-    write(destFileID, "Tipo: ", strlen("Tipo: "));
-    write(destFileID, &s_h->InfoF->Tipo, 1); 
-    write(destFileID, "\n", 1);
-
-    write(destFileID, "Tamaño del fichero origen: ", strlen("Tamaño del fichero origen: "));
-    write(destFileID, str, strlen(str));
-    write(destFileID, "\n", 1);
-
-    write(destFileID, "Tamaño del fichero comprimido: ", strlen("Tamaño del fichero comprimido: "));
-    write(destFileID, str, strlen(str));
-    write(destFileID, "\n", 1);
-
-    write(destFileID, s_h->RestoHeader, strlen(s_h->RestoHeader));
-    write(destFileID, "\n", strlen("\n"));
 
     int n;
     char buf[TAM_BUFFER];   
 
-    if ( (n = read(sourceFileID, buf, TAM_BUFFER)) > 0)
+    while ( (n = read(sourceFileID, buf, TAM_BUFFER)) > 0)
         write(destFileID, buf, n);
 
     close(sourceFileID);
