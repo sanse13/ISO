925495c9594ec8cef521a769a3a61dbb735d15a5
Adrián San Segundo <adriansanse@pc-1-128.customer.ask4.lan>
2020-02-15 21:54:33 +0100

    fixed mypack

--- mypack.c
+++ mypack.c
@@ -5,39 +5,7 @@
 #include <string.h>
 #include <sys/types.h>
 #include <errno.h>
-
-#define RESTO_HEADER (TAM_S_HEADER - TAM_S_INFO)
-#define TAM_S_HEADER    512
-#define MAX_FILE_NAME   256
-#define TAM_BUFFER      512
-#define TAM_S_INFO sizeof(struct s_info )
-
-#define ERR1 1
-#define ERR2 2
-#define ERR3 3
-#define ERR4 4
-
-#define MSG1 "Error en los argumentos\n"
-#define MSG2 "No existe el fichero de origen\n"
-#define MSG3 "No se dispone de permisos para esta operación. No se dispone de permisos para crear o modificar fich_destino\n"
-#define MSG4 "Otro tipo de errores\n"
-
-
-struct s_info {
-           char Tipo;                       // ='Z'
-           char Compri;                     // ='N' (por defecto) o 'Y'. Indica si los datos del  
-                                            // fichero están almacenados de forma comprimida
-           unsigned long TamOri;            // Tamanio original del fichero (sin comprimir)
-           unsigned long TamComp;           // Tamanio del fichero comprimido. Si los datos no estan
-                                            // comprimidos, su valor es el mismo de TamOri-
-           char FileName[MAX_FILE_NAME];    // Nombre del fichero origen 
-};
-
-struct  s_header {
-           struct s_info InfoF;                 // Información del fichero
-           char RestoHeader[RESTO_HEADER];       // ToDo...  
-};
-
+#include "util.h"
 
 int main(int argc, char* argv[]){
 
@@ -65,18 +33,16 @@ int main(int argc, char* argv[]){
         _exit(ERR3);
     }
 
-    //otros errores (ERR4)
-    //error 4 -> ________???
-
     strcpy(header.InfoF.FileName, argv[1]);
     header.InfoF.Compri = 'N';
     header.InfoF.Tipo = 'Z';
     header.InfoF.TamOri = lseek(sourceFileID, 0L, SEEK_END);
-    header.InfoF.TamComp = header.InfoF.TamOri; //sera modificado en futuras versiones
+    header.InfoF.TamComp = header.InfoF.TamOri; 
 
-    write(destFileID, "\n", 1);
+    lseek(sourceFileID, -0L, SEEK_SET);
 
     write(destFileID, &header, sizeof(header));
+
     int n;
     char buf[TAM_BUFFER];   
 
