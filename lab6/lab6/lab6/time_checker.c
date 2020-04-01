d23761b1c917e4c656c6cce8a9417baad999a1ea
Adrian <adrisanse.ss@gmail.com>
2020-04-01 18:16:25 +0200

    time_checker

--- lab6/lab6/lab6/time_checker.c
+++ lab6/lab6/lab6/time_checker.c
@@ -1,7 +1,6 @@
 /* time_checker.c */
 
-/* ./time_checker  nb_tests  min_time  max_time  program [parameters]  */
-/* probar si un programa se ejecuta o no en un intervalo de tiempo determinado */
+/* ./time_checker 3 2 6 sleep 10 */
 
 #include <sys/types.h>
 #include <unistd.h>
@@ -9,71 +8,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char const *argv[]) {
-  int nb_tests = atoi(argv[1]);
-  int min_time = atoi(argv[2]);
-  int max_time = atoi(argv[3]);
-  int clockid_min, childid, aux, id, tiempo_de_menos, t, aux_nb_tests, t_total;
-  aux_nb_tests = nb_tests;
+main(int argc, char *argv[])
+{
+   int childid, clockid, clockid2, id, aux;
+   long t, total;
+   int loops = atoi(argv[1]);
+   int menos = 0;
+   int mas = 0;
+   int ok = 0;
+   int minimo = atoi(argv[2]);
+   int maximo = atoi(argv[3]);
 
-  t_total = time(0);
-  if (aux_nb_tests == 0){
-    printf("Debe de ser mayor que cero el numero de pruebas\n");
-    return 0;
-  }
+   total = time(0);
 
-  while (aux_nb_tests > 0){
-    /* se realizara el numero de pruebas indicado*/
-    printf("Probando para %d segundos\n", min_time);
-    if ( (clockid_min = fork()) == 0)
-      sleep(min_time);
-    if ( (childid = fork()) == 0)
-      execvp(argv[4], &(argv[4]));
-    t = time(0);
-    id = wait(NULL);
-    if (id == childid) {   /* child finishes first; stop clock */
-       kill(clockid_min, SIGKILL);
-       wait(NULL);
-       aux = 0;
-    } else {   /* clock finishes first; force child to finish */
-       kill(childid, SIGKILL);
-       wait(NULL);
-       printf("--overtime\n");
-       aux = -1;
+   while(loops > 0){
+      
+  
+      if ((childid = fork()) == 0) execvp(argv[4], &(argv[4]));
+
+
+      t = time(0);
+      id = wait(NULL);
+      t = time(0) - t;
+      printf("TIME: %d\n", t);
+      if (t < minimo) menos++;
+      if (t > maximo) mas++;
+      if (t > minimo && t < maximo) ok++;
+      loops--;
    }
 
-   t = time(0) - t;
-   printf("--child time: %d\n", t);
-   printf("El programa ha sido %d/%d veces mas rapido de %d segundos\n\n\n", t, min_time, min_time);
-
-   printf("Probando para %d segundos\n", max_time);
-   if ( (clockid_min = fork()) == 0)
-     sleep(max_time);
-   if ( (childid = fork()) == 0)
-     execvp(argv[4], &(argv[4]));
-     t = time(0);
-     id = wait(NULL);
-     if (id == childid) {   /* child finishes first; stop clock */
-        kill(clockid_min, SIGKILL);
-        wait(NULL);
-        aux = 0;
-     } else {   /* clock finishes first; force child to finish */
-        kill(childid, SIGKILL);
-        wait(NULL);
-        printf("--overtime\n");
-        aux = -1;
-    }
-
-    t = time(0) - t;
-    printf("--child time: %d\n", t);
-    printf("El programa ha sido %d/%d mas veces lento de %d segundos\n\n\n", t, max_time, max_time);
-
-    /* y ahora el tiempo entre min y max */
-    float entre = max_time/min_time;
-    aux_nb_tests--;
-  }
-
-  t_total = time(0) - t_total;
-  printf("El tiempo total ha sido %d segundos\n", t_total);
-  return 0;
+  total = time(0) - total;
+  printf("too fast: %d\n", menos);
+  printf("too slow: %d\n", mas);
+  printf("ok: %d\n", ok);
+  printf("total: %d\n", total);
+  exit(aux);
 }
